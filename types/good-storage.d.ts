edd90e2bf53d52303bcfd0fa537f07845833e100
ken <634323580@qq.com>
2022-01-07 17:13:22 +0800

    fix: 修改good-storage声明文件

--- types/good-storage.d.ts
+++ types/good-storage.d.ts
@@ -1,18 +1,9 @@
+declare function set(key: string, value: any): void;
+declare function get<T>(key: string, def: T): T;
+declare function remove(key: string): void;
+declare function has(key: string): boolean;
+declare function clear(): void;
+declare function getAll(): any;
+declare function forEach(callback: (key: string, value: any) => any): any;
 
-
-declare module 'good-storage' {
-  interface Set {
-    (key: string, value: any): void
-  }
-  declare function set(key: string, value: any): void
-  declare function get<T>(key: string, def: T): T
-  declare function remove(key: string): void
-  declare function has(key: string): any
-  declare function clear(): void
-  declare function getAll(): any
-  declare function forEach(): any
-  declare const storage: {
-    set(key: string, value: any): void
-    get<T>(key: string, def: T): T
-  }
-}
+export { set, get, remove, has, clear, getAll, forEach };
